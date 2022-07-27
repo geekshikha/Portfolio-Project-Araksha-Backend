aa7d32d58edccdc8abd403f1de62a9830e89b96c
geekshikha <sssinghshikha8@gmail.com>
2022-07-27 18:10:03 +0200

    New Seeds added to categories

--- seeders/20220704134000-some-categories.js
+++ seeders/20220704134000-some-categories.js
@@ -6,27 +6,6 @@ module.exports = {
       "categories",
       [
         {
-          title: "Women",
-          image:
-            "https://images.unsplash.com/photo-1579838333277-8b1b1f9d7856?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGp3ZWxsZXJ5JTIwcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Kids",
-          image:
-            "https://media.istockphoto.com/photos/mother-helping-daughter-fasten-necklace-picture-id171102388?k=20&m=171102388&s=612x612&w=0&h=YRs1fV26T_fBW1aPZTqcXtQQ3oWR3mKflnC-Auzyzyk=",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "Men",
-          image:
-            "https://images.unsplash.com/photo-1611099144078-5596e87ef54c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjZ8fHdhbGxldHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
           title: "Pets",
           image:
             "https://images.unsplash.com/photo-1527526029430-319f10814151?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHBldCUyMGNvbGxhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
@@ -34,9 +13,31 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
-          title: "Seniors",
+          title: "Accessories",
           image:
-            "https://images.unsplash.com/photo-1490349708435-19d432984978?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2658&q=90",
+            "https://images.unsplash.com/photo-1579838333277-8b1b1f9d7856?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGp3ZWxsZXJ5JTIwcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Bikes",
+          image:
+            "https://images.unsplash.com/photo-1596372089183-5eb9aab2faaa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTg1fHxiaWtlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Trolleys",
+          image:
+            "https://www.bagageonline.nl/media/catalog/product/cache/8b0434a0f4e1877e35ccbb896e197ea6/r/k/rk-7801a-kleur-blauw-voorkant.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+
+        {
+          title: "Others",
+          image:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTANhTisHC2dAWFHNB2cvltlJWH85CYjPwyVg&usqp=CAU",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
--- seeders/20220704134014-some-devices.js
+++ seeders/20220704134014-some-devices.js
@@ -6,86 +6,6 @@ module.exports = {
       "devices",
       [
         {
-          title: "Ring",
-          description: "Sterling • Silver Gemstone • Bypass Ring",
-          price: 162,
-          image:
-            "https://images.unsplash.com/photo-1552234914-13f6e51124ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGp3ZWxsZXJ5JTIwcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 1,
-        },
-        {
-          title: "Necklace",
-          description: "Gold • Cubic • Zirconia Necklace",
-          price: 424,
-          image:
-            "https://images.unsplash.com/photo-1599643477877-530eb83abc8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5lY2tsYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 1,
-        },
-        {
-          title: "Earring",
-          description: "Round • Leverback • Dangle Earrings",
-          price: 224,
-          image:
-            "https://media.istockphoto.com/photos/white-gold-emerald-earrings-picture-id1365870463?k=20&m=1365870463&s=612x612&w=0&h=wbxj_erJYiyF15NjELh7eB4lZW__1UxD58qGcZVyxkw=",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 1,
-        },
-        {
-          title: "Bracelet",
-          description: "Round White • Pink Freshwater • Cultured Pearl",
-          price: 100,
-          image:
-            "https://media.istockphoto.com/photos/gold-bracelets-accumulation-and-ring-on-a-woman-wrist-wearing-a-white-picture-id1348544609?k=20&m=1348544609&s=612x612&w=0&h=3Q-LiDKLE-xTSn5MroEJOsth-33Mj144W4YCCoSdTyY=",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 1,
-        },
-        {
-          title: "Pendent",
-          description: "Beautiful • BlueRuby Pendent",
-          price: 224,
-          image:
-            "https://media.istockphoto.com/photos/amulet-of-evil-eye-picture-id1151053898?k=20&m=1151053898&s=612x612&w=0&h=T-uaVtrWI892eyGac3ur6RA7-jU8zy1aJxQFqPTQ8-I=",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 2,
-        },
-        {
-          title: "Bracelet",
-          description: "Multicolored Spacer • Faux Round Laege • beads",
-          price: 124,
-          image:
-            "https://media.istockphoto.com/photos/childrens-multicolored-manicure-picture-id487745338?k=20&m=487745338&s=612x612&w=0&h=dUVI1y1N4IDfjjVhMvY9g9IX440TAkzuLvpphLtxngY=",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 2,
-        },
-        {
-          title: "Wallet",
-          description: "Hybrid RFID • top-quality leather",
-          price: 224,
-          image:
-            "https://images.unsplash.com/photo-1611099024089-7f57ca9aca7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdhbGxldHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 3,
-        },
-        {
-          title: "Ring",
-          description: "Vintage Biker Signet • luxurious texture",
-          price: 424,
-          image:
-            "https://images.unsplash.com/photo-1612285127364-58ede3fa1686?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njl8fG1lbnMlMjByaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          categoryId: 3,
-        },
-        {
           title: "Collar",
           description: "Pretty • Collar",
           price: 324,
@@ -93,7 +13,7 @@ module.exports = {
             "https://images.unsplash.com/photo-1610714520720-ba92ae46e0e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sbGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
           createdAt: new Date(),
           updatedAt: new Date(),
-          categoryId: 4,
+          categoryId: 1,
         },
         {
           title: "Shoes",
@@ -103,7 +23,7 @@ module.exports = {
             "https://media.istockphoto.com/photos/small-french-bulldog-dog-puppy-with-umbrella-and-rain-boots-picture-id1363594241?k=20&m=1363594241&s=612x612&w=0&h=8U1mUdP8wFqM2o-lTfmltJxRBLtlI-XJ2mSNBWA0PPQ=",
           createdAt: new Date(),
           updatedAt: new Date(),
-          categoryId: 4,
+          categoryId: 1,
         },
         {
           title: "NeckPiece",
@@ -113,22 +33,106 @@ module.exports = {
             "https://media.istockphoto.com/photos/womans-hand-holding-antique-locket-with-photos-of-children-and-pet-picture-id1091800452?s=612x612",
           createdAt: new Date(),
           updatedAt: new Date(),
+          categoryId: 1,
+        },
+        {
+          title: "Ring",
+          description: "Sterling • Silver Gemstone • Bypass Ring",
+          price: 162,
+          image:
+            "https://images.unsplash.com/photo-1552234914-13f6e51124ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGp3ZWxsZXJ5JTIwcmluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 2,
+        },
+        {
+          title: "Necklace",
+          description: "Gold • Cubic • Zirconia Necklace",
+          price: 424,
+          image:
+            "https://images.unsplash.com/photo-1599643477877-530eb83abc8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5lY2tsYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 2,
+        },
+
+        {
+          title: "Bracelet",
+          description: "Round White • Pink Freshwater • Cultured Pearl",
+          price: 100,
+          image:
+            "https://media.istockphoto.com/photos/gold-bracelets-accumulation-and-ring-on-a-woman-wrist-wearing-a-white-picture-id1348544609?k=20&m=1348544609&s=612x612&w=0&h=3Q-LiDKLE-xTSn5MroEJOsth-33Mj144W4YCCoSdTyY=",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 2,
+        },
+
+        {
+          title: "Ring",
+          description: "Vintage Biker Signet • luxurious texture",
+          price: 424,
+          image:
+            "https://images.unsplash.com/photo-1612285127364-58ede3fa1686?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njl8fG1lbnMlMjByaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 2,
+        },
+
+        {
+          title: "Metal Bike",
+          description: "Vintage Biker Signet • luxurious texture",
+          price: 424,
+          image:
+            "https://images.unsplash.com/photo-1591047139334-337807f2b3e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fGJpa2V8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 3,
+        },
+        {
+          title: "Electric Bike",
+          description: "Electric Bike  • No gear",
+          price: 424,
+          image:
+            "https://images.unsplash.com/photo-1620711973925-1f67a54528c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fGVsZWN0cmljJTIwYmlrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 3,
+        },
+        {
+          title: "Decent Transporto",
+          description: "One Hand Luggage Trolley  • 55 Light Blue",
+          price: 59,
+          image:
+            "https://www.bagageonline.nl/media/catalog/product/cache/a19cf5428adfa373f1b5c6af26e83736/r/k/rk-7801a-kleur-blauw-voorkant.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
           categoryId: 4,
         },
         {
-          title: "Walking Sticks",
-          description: "Easyly Accessable",
+          title: "Samsonite Airea Spinner",
+          description: "Strict Dark Blue  • Double adjustable drawbar ",
+          price: 199,
+          image:
+            "https://www.bagageonline.nl/media/catalog/product/cache/a19cf5428adfa373f1b5c6af26e83736/1/3/133623-124710.jpg",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          categoryId: 4,
+        },
+        {
+          title: "Wallet",
+          description: "Hybrid RFID • top-quality leather",
           price: 224,
           image:
-            "https://images.unsplash.com/photo-1644043975612-5f8176666c88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHdhbGtpbmclMjBvbGQlMjBwZXJzb25zJTIwc3RpY2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            "https://images.unsplash.com/photo-1611099024089-7f57ca9aca7f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHdhbGxldHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
           createdAt: new Date(),
           updatedAt: new Date(),
           categoryId: 5,
         },
+
         {
           title: "Hiking Sticks",
           description: "Hike Easily",
-          price: 224,
+          price: 124,
           image:
             "https://images.unsplash.com/photo-1592388748465-8c4dca8dd703?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2Fsa2luZyUyMHN0aWNrc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
           createdAt: new Date(),
