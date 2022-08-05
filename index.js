6b7386851581c1cdea09bee9c82615de11d325a5
geekshikha <sssinghshikha8@gmail.com>
2022-08-05 20:27:47 +0200

    Added netlify url to socketIO

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://admin.socket.io/"],
+    origin: ["https://araksha.netlify.app", "https://admin.socket.io/"],
     methods: ["GET", "POST"],
   },
 });
